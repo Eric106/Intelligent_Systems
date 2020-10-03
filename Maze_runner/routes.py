e31e3734ed5f0d7dde33f4bc34714a6affdb8fb7
Eric106 <ercv106@hotmail.com>
2020-10-03 05:53:04 -0500

    03/10/2020 05:53:04

--- Maze_runner/routes.py
+++ Maze_runner/routes.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from utils.main import a_star, brute_force
 from utils.modules.mazeGenerator import generator
-from os import system
 
 app = Flask(__name__)
 
@@ -9,15 +8,13 @@ app = Flask(__name__)
 def home():
     return  render_template('index.html')
 
-@app.route('/A')
+@app.route('/A')   
 def A_Search():
-    system("del /static/a.gif")
     a_star(generator(30, 20))
     return render_template('a.html')
 
 @app.route('/Brute')
 def Brute():
-    system("del /static/brute.gif")
     brute_force(generator(30, 20))
     return render_template('brute.html')
 
@@ -26,5 +23,5 @@ def Video():
     return render_template('video.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
     
