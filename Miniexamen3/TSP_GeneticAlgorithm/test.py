d49239216bd1b0e27c7769917d04febc2ea81f18
Eric106 <ercv106@hotmail.com>
2020-10-15 13:59:20 -0500

    15/10/2020 13:59:19

---
+++ Miniexamen3/TSP_GeneticAlgorithm/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+from geneticalgorithm import geneticalgorithm as ga
+
+def f(X):
+    return np.sum(X)
+    
+    
+varbound=np.array([[0,10]]*3)
+
+# print(varbound)
+model=ga(function=f,dimension=3,variable_type='real',variable_boundaries=varbound)
+
+model.run()
+print(model.report)
+print(model.output_dict)
